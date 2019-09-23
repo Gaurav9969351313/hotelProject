43087b7cb66cddcfdc7b45d20c514e9373c1187e
gaurav9969351313 <gauravtalele@gmail.com>
2019-09-23 12:46:50 +0530

    Adding Rental Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,22 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import {  RentalModule } from "./rental/rental.module";
+import { Routes, RouterModule } from '@angular/router';
+import { RentalComponent } from './rental/rental.component';
+
+const routes: Routes = [
+  { path:'', component:RentalComponent }
+];
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
+    RentalModule,
+    RouterModule.forRoot(routes),
     AppRoutingModule
   ],
   providers: [],
